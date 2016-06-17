75470665933f38dd8bccb12fb3e4468b957f3082
Andreas Klebinger <e1426266@student.tuwien.ac.at>
2016-06-17 22:57:00 +0200

    Started creating proper testcases

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,11 +2,19 @@ import LVM
 import LuaObjects
 import LuaLoader
 import Test.HUnit
+import qualified LRT
+import qualified Control.Monad              as Monad
+import qualified LVM
+import qualified Parser
+import qualified Data.ByteString as BS
+import qualified Data.Attoparsec.ByteString as PBS
+
 
 main :: IO ()
 main = do
-  putStrLn "Test suite not yet implemented"
-  runTestTT operatorTests
+  fmap print (testFile "test/testFiles/luac.out" [LONil])
+  --putStrLn "Test suite not yet implemented"
+  --runTestTT operatorTests
   return ()
 
 operatorTests = TestList [
@@ -19,6 +27,26 @@ wrapStackTest stack instruction =  LuaState (LuaExecutionThread (LuaFunctionInst
 testStack :: LuaMap -> LuaInstruction -> LuaMap -> Bool
 testStack stack instruction expectedStack =
   let state = wrapStackTest stack instruction
-
   in
   lGetStateStack (execPureOP state) == expectedStack
+
+lcompareResults :: [LuaObject] -> LuaMap -> [(Bool, LuaObject, LuaObject)]
+lcompareResults expected results = zipWith compareLO expected (toList results)
+
+testFile :: FilePath -> [LuaObject] -> IO [(Bool, LuaObject, LuaObject)]
+testFile path objs = do
+  let vmResult = runLuaCode path
+  stack <- fmap lGetStateStack vmResult
+  return $ lcompareResults objs stack
+
+
+runLuaCode :: FilePath -> IO LuaState
+runLuaCode path = do
+  fileContent <- BS.readFile path :: IO BS.ByteString
+  let chunk = either (error "Failed to parse binary file") id $ LuaLoader.loadLua fileContent -- :: IO (Either String Parser.LuaBinaryFile)
+  vm <- LVM.startExecution $ Parser.getTopFunction chunk :: IO LuaState
+  LVM.runLuaFunction $ return vm
+
+compareLO :: LuaObject -> LuaObject -> (Bool, LuaObject, LuaObject)
+compareLO a b =
+  (a == b, a, b)
