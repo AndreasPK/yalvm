583dd00acfa884fd70c368d348541c2ff26b5cd4
Andreas Klebinger <e1426266@student.tuwien.ac.at>
2016-06-28 18:42:45 +0200

    Removed pause from lua compile script

--- test/testFiles/compile.bat
+++ test/testFiles/compile.bat
@@ -2,6 +2,5 @@ FOR %%X in (".\*.lua") DO (
 	luac5.1 -o %%~nX.luac %%~nX.lua
 	luac5.1 -l %%~nX.lua  > %%~nX.listing
 	)
-	
+
 echo Done
-pause
--- test/testFiles/test.lua
+++ test/testFiles/test.lua
@@ -1,7 +1,15 @@
-function f(x)
-  return 1,2
+function fac(x)
+  if x == 1 then
+    return x
+  else
+    return x * fac(x-1)
+  end
 end
 
-local x = f(1)
-
-return ff(10)
+for i=1,10000 do
+  --print("Iteration" ++ i)
+  print(fac(25))
+  print("Loop")
+end
+print("end")
+print(fac(25))
