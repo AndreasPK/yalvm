6f416b024d4f550926a0433a4c793f4d6d7d7a7d
Andreas Klebinger <e1426266@student.tuwien.ac.at>
2016-07-30 20:14:39 +0200

    Benchmark changed

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -3,8 +3,8 @@ import Criterion.Types
 import LVM
 
 
-main = defaultMainWith defaultConfig { timeLimit = 120 } [
-  bgroup "yalvm" [ bench "count"  $ whnfIO $ LVM.runLuaCode "test\\testFiles\\counter.luac"
+main = defaultMainWith defaultConfig { timeLimit = 30 } [
+  bgroup "yalvm" [ bench "count"  $ whnfIO $ LVM.runLuaCode "test\\testFiles\\counter_perf.luac"
                , bench "fac_perv"  $ whnfIO $ LVM.runLuaCode "test\\testFiles\\fac_perf.luac"
                --, bench "count"  $ whnf $ LVM.testFile "counter.luac"
                --, bench "count"  $ whnf $ LVM.testFile "counter.luac"
