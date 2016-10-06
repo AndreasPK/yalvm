e3d8cf3c62b68a142da5c3c18f86a8dff7a82937
AndreasPK <klebinger.andreas@gmx.at>
2016-10-06 19:44:15 +0200

    Adjusted parser to be compatible with 32bit and 64bit compiled lua code

--- src/Parser.hs
+++ src/Parser.hs
@@ -12,9 +12,10 @@ import Data.ByteString as B
 import Data.ByteString.Char8 as B8
 import qualified Data.Bits as Bits
 import Data.Binary.IEEE754
-import Debug.Trace
 import Data.Vector.Unboxed as VU
 import Data.Vector as V
+
+import Debug.Trace
 
 
 luaHeaderDefinition :: ByteString
@@ -39,19 +40,43 @@ loadLuaChunk = luaBinaryFile
 luaBinaryFile :: PBS.Parser LuaBinaryFile
 luaBinaryFile = do
   header <- loadHeader
-  functions <- luaFunctionHeader
+  functions <- luaFunctionHeader header
   return $ LuaBinaryFile header [functions]
+
+-- | given the size of Integers return a parser reading such a integer
+getSize :: (Num sizeT) => LuaHeader -> PBS.Parser sizeT
+getSize lh =
+  let s = lhSizeT lh
+  in
+  case s of
+      4 -> fromIntegral <$> anyWord32le
+      8 -> fromIntegral <$> anyWord64le
+      _ -> error "Unknown word size"
+
+-- | given the size of Integers return a parser reading such a integer
+getInt :: (Num int) => LuaHeader -> PBS.Parser int
+getInt lh =
+  let s = lhIntSize lh
+  in
+  case s of
+      4 -> fromIntegral <$> anyWord32le
+      8 -> fromIntegral <$> anyWord64le
+      _ -> error "Unknown word size"
 
 -- | Header of chunk, with Major Minor version and VM compitability to reference
 --   Implementation.
 -- LuaHeader major minor compatibel
-data LuaHeader = LuaHeader Int Int Bool
-  Word8 -- 1 = little endian
-  Word8 -- int size on vm
-  Word8 -- size_t size
-  Word8 -- op code Size
-  Word8 -- (8) lua number size
-  Word8 -- 0 == floating point
+data LuaHeader = LuaHeader
+  { _lh1 :: Int
+  , _lh2 ::Int
+  , _lh3 :: Bool
+  , _lh4 :: Word8 -- 1 = little endian
+  , lhIntSize :: Word8 -- int size on vm
+  , lhSizeT :: Word8 -- size_t size
+  , _lh7 :: Word8 -- op code Size
+  , _lh8 :: Word8 -- (8) lua number size
+  , _lh9 :: Word8 -- 0 == floating point
+  }
   deriving (Eq, Show)
 
 --Parses the lua 5.1 (and 5.2) file header
@@ -80,8 +105,8 @@ decodeLuaVersion w =
 
 data LuaFunctionHeader = LuaFunctionHeader
   {  fhName :: LuaString -- name
-  ,  fhStart :: Word32 --line defined
-  ,  fhEnd :: Word32 --line end
+  ,  fhStart :: Int --line defined
+  ,  fhEnd :: Int --line end
   ,  fhUpvalueCount :: Word8 --upvalueCount
   ,  fhParameterCount :: Word8 --parameterCount
   ,  fhVarargFlag :: Word8 --varargFlag
@@ -96,22 +121,22 @@ data LuaFunctionHeader = LuaFunctionHeader
   deriving (Eq, Show, Ord)
 
 -- | Read a function header from a lua chunk, assumes 4 byte ints, opcodes and sizeOf fields as well as little endian
-luaFunctionHeader :: PBS.Parser LuaFunctionHeader
-luaFunctionHeader = do
-  sourceName <- luaString
-  startLine <- anyWord32le
-  endLine <- anyWord32le
+luaFunctionHeader :: LuaHeader -> PBS.Parser LuaFunctionHeader
+luaFunctionHeader lh = do
+  sourceName <- luaString lh
+  startLine <- getInt lh
+  endLine <- getInt lh
   upvalueCount <- anyWord8
   parameterCount <- anyWord8
   varargFlag <- anyWord8 --not implemented
   maxStackSize <- anyWord8
-  instList <- luaInstructionList
-  constList <- luaConstList
-  functionList <- luaFunctionList
+  instList <- luaInstructionList lh
+  constList <- luaConstList lh
+  functionList <- luaFunctionList lh
   --debug info
-  linePositions <- luaLinePositionList
-  localList <- luaLocalList
-  upvalues <- luaUpvalueList
+  linePositions <- luaLinePositionList lh
+  localList <- luaLocalList lh
+  upvalues <- luaUpvalueList lh
   return $
     LuaFunctionHeader sourceName startLine endLine upvalueCount
       parameterCount varargFlag maxStackSize instList constList functionList
@@ -119,10 +144,10 @@ luaFunctionHeader = do
 
 data LuaInstructionList = LuaInstructionList { lilLength :: Word32, lilInstructions :: !(VU.Vector Word32) } deriving (Eq, Show, Ord)
 
-luaInstructionList :: PBS.Parser LuaInstructionList
-luaInstructionList =
+luaInstructionList :: LuaHeader -> PBS.Parser LuaInstructionList
+luaInstructionList lh =
   do
-  size <- anyWord32le
+  size <- getInt lh
   instructions <- PBS.count (fromIntegral size) anyWord32le
   return $ LuaInstructionList size $ VU.fromList instructions
   PBS.<?> "InstructionList"
@@ -133,17 +158,17 @@ data LuaConst = LuaNil |
   LuaConstString LuaString
   deriving (Eq, Show, Ord)
 
-data LuaConstList = LuaConstList Word32 !(V.Vector LuaConst) deriving (Eq, Show, Ord)
+data LuaConstList = LuaConstList Int !(V.Vector LuaConst) deriving (Eq, Show, Ord)
 
-luaConstList :: PBS.Parser LuaConstList
-luaConstList = do
-  x <- anyWord32le
-  y <- PBS.count (fromIntegral x) luaConst
+luaConstList :: LuaHeader -> PBS.Parser LuaConstList
+luaConstList lh = do
+  x <- getInt lh
+  y <- PBS.count (fromIntegral x) $ luaConst lh
   return $ LuaConstList x $! V.fromList y
   PBS.<?> "ConstList"
 
-luaConst :: PBS.Parser LuaConst
-luaConst =
+luaConst :: LuaHeader -> PBS.Parser LuaConst
+luaConst ws =
   do
   t <- anyWord8
   case t of
@@ -155,7 +180,7 @@ luaConst =
       x <- anyWord64le
       return $ LuaConstNumber $ wordToDouble  x
     4 -> do
-      x <- luaString
+      x <- luaString ws
       return $ LuaConstString x
   PBS.<?> "Const"
 
@@ -163,22 +188,22 @@ luaConst =
 type LuaFunctionList = [LuaFunctionHeader]
 --  deriving (Eq, Show)
 
-luaFunctionList :: PBS.Parser LuaFunctionList
-luaFunctionList =
+luaFunctionList :: LuaHeader -> PBS.Parser LuaFunctionList
+luaFunctionList ws =
   do
-    size <- anyWord32le
+    size <- getInt ws
     let s = fromIntegral size
-    PBS.count s luaFunctionHeader
+    PBS.count s $ luaFunctionHeader ws
   PBS.<?> "FunctionList"
 
 -- the Nth instruction was genereated based on the n'th line of code in the origin file
 type LuaInstructionPositionList = [Int]
 
-luaLinePositionList :: PBS.Parser LuaInstructionPositionList
-luaLinePositionList =
+luaLinePositionList :: LuaHeader -> PBS.Parser LuaInstructionPositionList
+luaLinePositionList ws =
   do
-    size <- anyWord32le
-    positions <- PBS.count (fromIntegral size) anyWord32le
+    size <- getInt ws
+    positions <- PBS.count (fromIntegral size) $ getInt ws
     return $ Prelude.map fromIntegral positions
 
 -- | LuaLocalList size localList
@@ -186,23 +211,23 @@ data LuaLocalList = LuaLocalList
   Int [LuaLocal]
   deriving (Eq, Show, Ord)
 
-luaLocalList :: PBS.Parser LuaLocalList
-luaLocalList =
+luaLocalList :: LuaHeader -> PBS.Parser LuaLocalList
+luaLocalList ws =
   do
-    size <- anyWord32le
+    size <- getInt ws
     let s = fromIntegral size
-    locals <- PBS.count s luaLocal
+    locals <- PBS.count s $ luaLocal ws
     return $ LuaLocalList s []--locals
   PBS.<?> "luaLocalList"
 
 data LuaLocal = LuaLocal LuaString Int Int deriving (Show, Eq, Ord)
 
-luaLocal :: PBS.Parser LuaLocal
-luaLocal =
+luaLocal :: LuaHeader -> PBS.Parser LuaLocal
+luaLocal lh =
   do
-    name <- luaString
-    startOfScope <- anyWord32le
-    endOfScope <- anyWord32le
+    name <- luaString lh
+    startOfScope <- getInt lh
+    endOfScope <- getInt lh
     return $ LuaLocal name (fromIntegral startOfScope) (fromIntegral endOfScope)
   PBS.<?> "luaLocal"
 
@@ -210,20 +235,20 @@ data LuaUpvalue = LuaUpvalue LuaString deriving (Eq, Show, Ord)
 
 data LuaUpvalueList = LuaUpvalueList Int [LuaUpvalue] deriving (Eq, Show, Ord)
 
-luaUpvalueList :: PBS.Parser LuaUpvalueList
-luaUpvalueList =
+luaUpvalueList :: LuaHeader -> PBS.Parser LuaUpvalueList
+luaUpvalueList ws =
   do
-    size <- anyWord32le
+    size <- getInt ws --anyWord32le
     let s = fromIntegral size
-    upvalues <- PBS.count s luaString
+    upvalues <- PBS.count s $ luaString ws
     return $ LuaUpvalueList s $ fmap LuaUpvalue upvalues
   PBS.<?> "luaUpvalueList"
 
 data LuaString = LuaString Int String deriving (Show, Eq, Ord)
 
-luaString :: PBS.Parser LuaString
-luaString = do
-  l <- anyWord32le
+luaString :: LuaHeader -> PBS.Parser LuaString
+luaString ws = do
+  l <- getSize ws --anyWord32le
   if l == 0 then return $ LuaString 0 []
     else
       do
